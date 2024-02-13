8ee873ea1b63edc7d4b63a3c018f9a9101d7c0b9
Oleksii Aleshchenko <alexey.aleshchenko@gmail.com>
2024-02-13 16:06:15 +0200

    edit Main.java (by IO-34)

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,34 @@
+//import static java.lang.System.out;
+//import static java.lang.System.setOut;
+
 public class Main {
+    byte i;
+
     public static void main(String[] args) {
         System.out.println("Hello and welcome!");
 
-        int a = 1;
+        var a = 10;
+        int b = 0xA;
+        int d = 010;
+        var f = 0.1;
+
+//        float a = /*(int) */0.1;
+
+        System.out.println(b);
+        System.out.println(a == b);
+
+        System.out.println(1/(float)2);
+
+        char c = 'a';
+
         System.out.println(++a);
         System.out.println(++a);
 
-        for (int i = 1; i <= 5; ++i) {
+//        todo "additional" ("extra"?) code
+        byte i;
+        for (i = 1; i > 0; i++) {
             System.out.println("i = " + i);
         }
+        System.out.println("i = " + i);
     }
 }
